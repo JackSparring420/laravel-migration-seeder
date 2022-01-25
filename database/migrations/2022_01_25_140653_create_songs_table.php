25124f1caa69f3be910cbf2fd3593bdedd2d974c
JackSparring420 <emanuele.arconte94@gmail.com>
2022-01-25 15:41:33 +0100

    completed

---
+++ database/migrations/2022_01_25_140653_create_songs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('album_id') -> unsigned();
+            $table->string('nome');
+            $table->time('durata');
+            $table->string('genere');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
---
+++ database/migrations/2022_01_25_140706_create_albums_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('artist_id') -> unsigned();
+            $table->string('nome');
+            $table->date('data-uscita');
+            $table->time('durata');
+            $table->string('numero-canzoni');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
